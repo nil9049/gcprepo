765429e299e8b00ca27502b09b300fd7de85a3e4
nil9049 <nghuge9049@gmail.com>
2024-11-30 01:29:13 +0530

    asds

--- ansible/jenkins.groovy
+++ ansible/jenkins.groovy
@@ -1,37 +1,41 @@
 pipeline {
     agent any
-
-    environment {
-        ANSIBLE_INVENTORY = 'ansible/inventory'  // Relative path to inventory file
-        PLAYBOOK = 'ansible/playbook.yml'         // Playbook file name
-        ANSIBLE_HOST_KEY_CHECKING = 'False'
-
+    parameters {
+        choice(name: 'PACKAGES', choices: ['apache2', 'nginx', 'mysql', 'postgresql'], description: 'Select a package to install')
+        choice(name: 'SERVICES', choices: ['apache2', 'nginx', 'mysql', 'postgresql'], description: 'Select a service to restart')
+        choice(name: 'PLAYBOOK', choices: ['install_packages', 'multiple_packages'], description: 'Select the playbook to execute')
     }
-
+    environment {
+        ANSIBLE_INVENTORY = 'ansible/inventory'  // Define the inventory path
+    }
     stages {
-        stage('Checkout') {
+        stage('Clone Repository') {
             steps {
-                // Checkout the repository from GitHub
-                git branch: 'main', url: 'https://github.com/nil9049/gcprepo.git'
+                git branch: 'main',
+                    url: 'https://github.com/nil9049/gcprepo.git'
             }
         }
-
         stage('Run Ansible Playbook') {
             steps {
                 script {
-                    // Execute the Ansible playbook
-                    sh "ansible-playbook -i ${ANSIBLE_INVENTORY} ${PLAYBOOK}"
+                    // Map the selected playbook to its full path
+                    def playbookMap = [
+                        'install_packages': 'ansible/playbook.yml',    // Corrected the typo here
+                        'multiple_packages': 'ansible/configure_services.yml'
+                    ]
+                    
+                    // Fetch the selected package and service
+                    def package = params.PACKAGES
+                    def service = params.SERVICES
+                    def playbookPath = playbookMap[params.PLAYBOOK]  // Get the mapped playbook path
+
+                    // Execute the selected playbook with extra-vars
+                    sh """
+                    ansible-playbook -i ${ANSIBLE_INVENTORY} ${playbookPath} \
+                    --extra-vars "package=${package} service=${service}"
+                    """
                 }
             }
-        }
-    }
-
-    post {
-        success {
-            echo 'Ansible playbook executed successfully!'
-        }
-        failure {
-            echo 'Ansible playbook execution failed.'
         }
     }
 }
