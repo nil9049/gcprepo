79b61d1ba9f630ea1e70c432e480f09774b7abb3
nil9049 <nghuge9049@gmail.com>
2024-11-30 00:34:00 +0530

    sd

---
+++ ansible/jenkins.groovy
@@ -0,0 +1,35 @@
+pipeline {
+    agent any
+
+    environment {
+        ANSIBLE_INVENTORY = 'inventory'  // Relative path to inventory file
+        PLAYBOOK = 'playbook.yml'         // Playbook file name
+    }
+
+    stages {
+        stage('Checkout') {
+            steps {
+                // Checkout the repository from GitHub
+                git branch: 'main', url: 'https://github.com/nil9049/gcprepo.git'
+            }
+        }
+
+        stage('Run Ansible Playbook') {
+            steps {
+                script {
+                    // Execute the Ansible playbook
+                    sh "ansible-playbook -i ${ANSIBLE_INVENTORY} ${PLAYBOOK}"
+                }
+            }
+        }
+    }
+
+    post {
+        success {
+            echo 'Ansible playbook executed successfully!'
+        }
+        failure {
+            echo 'Ansible playbook execution failed.'
+        }
+    }
+}
