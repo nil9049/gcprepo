635dc3fc89989d2581d302e88e3cb5a8df9f34c1
nil9049 <nghuge9049@gmail.com>
2024-11-30 02:06:53 +0530

    asdds

--- ansible/jenkins.groovy
+++ ansible/jenkins.groovy
@@ -1,48 +1,57 @@
 pipeline {
     agent any
-    parameters {
-        choice(name: 'PLAYBOOK', choices: ['install_packages', 'multiple_packages'], description: 'Select the playbook to execute')
-
-        // Active choice for PACKAGES that updates based on the PLAYBOOK selection
-        activeChoiceParam(name: 'PACKAGES') {
-            description('Select a package to install')
-            filterable()
-            groovyScript {
-                script("""
-                    if (PLAYBOOK == 'install_packages') {
-                        return ['apache2']
-                    } else if (PLAYBOOK == 'multiple_packages') {
-                        return ['nginx', 'mysql', 'postgresql']
-                    } else {
-                        return []
-                    }
-                """)
-                fallbackScript('return ["apache2"]')
-            }
-        }
-
-        // Active choice for SERVICES that updates based on the PLAYBOOK selection
-        activeChoiceParam(name: 'SERVICES') {
-            description('Select a service to restart')
-            filterable()
-            groovyScript {
-                script("""
-                    if (PLAYBOOK == 'install_packages') {
-                        return ['apache2']
-                    } else if (PLAYBOOK == 'multiple_packages') {
-                        return ['apache2', 'nginx', 'mysql', 'postgresql']
-                    } else {
-                        return []
-                    }
-                """)
-                fallbackScript('return ["apache2"]')
-            }
-        }
-    }
     environment {
         ANSIBLE_INVENTORY = 'ansible/inventory'  // Path to your inventory file
     }
+    parameters {
+        choice(name: 'PLAYBOOK', choices: ['install_packages', 'multiple_packages'], description: 'Select the playbook to execute')
+    }
     stages {
+        stage('Define Parameters') {
+            steps {
+                script {
+                    // Active Choice for PACKAGES
+                    properties([
+                        parameters([
+                            activeChoiceParam(name: 'PACKAGES') {
+                                description('Select a package to install')
+                                filterable()
+                                groovyScript {
+                                    script("""
+                                        if (PLAYBOOK == 'install_packages') {
+                                            return ['apache2']
+                                        } else if (PLAYBOOK == 'multiple_packages') {
+                                            return ['nginx', 'mysql', 'postgresql']
+                                        } else {
+                                            return []
+                                        }
+                                    """)
+                                    fallbackScript('return ["apache2"]')
+                                }
+                            },
+
+                            // Active Choice for SERVICES
+                            activeChoiceParam(name: 'SERVICES') {
+                                description('Select a service to restart')
+                                filterable()
+                                groovyScript {
+                                    script("""
+                                        if (PLAYBOOK == 'install_packages') {
+                                            return ['apache2']
+                                        } else if (PLAYBOOK == 'multiple_packages') {
+                                            return ['apache2', 'nginx', 'mysql', 'postgresql']
+                                        } else {
+                                            return []
+                                        }
+                                    """)
+                                    fallbackScript('return ["apache2"]')
+                                }
+                            }
+                        ])
+                    ])
+                }
+            }
+        }
         stage('Run Ansible Playbook') {
             steps {
                 script {
