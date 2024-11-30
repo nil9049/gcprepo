c227dce4d1785c83ead23a256376e0846a16ea94
nil9049 <nghuge9049@gmail.com>
2024-11-30 18:26:12 +0530

    pb3

--- ansible/jenkins3.groovy
+++ ansible/jenkins3.groovy
@@ -7,6 +7,8 @@ pipeline {
         string(name: 'DISK_NAME', defaultValue: '', description: 'Disk Name')
         string(name: 'DISK_SIZE', defaultValue: '', description: 'Disk Size (e.g., 10GB)')
         string(name: 'ZONE', defaultValue: '', description: 'GCP Zone')
+        string(name: 'CUSTOM_MOUNT_POINT', defaultValue: '/mnt/custom', description: 'Custom Mount Point (e.g., /mnt/data)')
+
     }
 
     environment {
@@ -21,7 +23,7 @@ pipeline {
                     sh '''
                     echo $GOOGLE_CREDENTIALS  > ${WORKSPACE}/gcp-key.json
                     export GOOGLE_APPLICATION_CREDENTIALS=${WORKSPACE}/gcp-key.json
-                    ansible-playbook create_and_attach_disk.yml --extra-vars "project_id=${params.PROJECT_ID} vm_name=${params.VM_NAME} disk_name=${params.DISK_NAME} disk_size=${params.DISK_SIZE} zone=${params.ZONE}"
+                    ansible-playbook -i inventory playbook3.yml --extra-vars "project_id=${params.PROJECT_ID} vm_name=${params.VM_NAME} disk_name=${params.DISK_NAME} disk_size=${params.DISK_SIZE} zone=${params.ZONE} custom_mount_point=$CUSTOM_MOUNT_POINT"
                     '''
                 }
             }
