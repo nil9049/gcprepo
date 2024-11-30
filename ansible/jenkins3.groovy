9f0a3909d1d5312f4a80c74aeb408e5854b24158
nil9049 <nghuge9049@gmail.com>
2024-11-30 18:13:13 +0530

    pb3

---
+++ ansible/jenkins3.groovy
@@ -0,0 +1,36 @@
+pipeline {
+    agent any
+
+    parameters {
+        string(name: 'PROJECT_ID', defaultValue: '', description: 'GCP Project ID')
+        string(name: 'VM_NAME', defaultValue: '', description: 'VM Name to attach disk to')
+        string(name: 'DISK_NAME', defaultValue: '', description: 'Disk Name')
+        string(name: 'DISK_SIZE', defaultValue: '', description: 'Disk Size (e.g., 10GB)')
+        string(name: 'ZONE', defaultValue: '', description: 'GCP Zone')
+    }
+
+    environment {
+        GOOGLE_CREDENTIALS = credentials('my-key')  // Jenkins credential for GCP service account
+    }
+
+    stages {
+        stage('Run Ansible Playbook to Create and Attach Disk') {
+            steps {
+                script {
+                    // Decode the Base64-encoded GCP service account credentials and save them to a file
+                    sh '''
+                    echo $GOOGLE_CREDENTIALS | base64 --decode > ${WORKSPACE}/gcp-key.json
+                    export GOOGLE_APPLICATION_CREDENTIALS=${WORKSPACE}/gcp-key.json
+                    ansible-playbook create_and_attach_disk.yml --extra-vars "project_id=${params.PROJECT_ID} vm_name=${params.VM_NAME} disk_name=${params.DISK_NAME} disk_size=${params.DISK_SIZE} zone=${params.ZONE}"
+                    '''
+                }
+            }
+        }
+    }
+
+    post {
+        always {
+            cleanWs()  // Clean up workspace
+        }
+    }
+}
